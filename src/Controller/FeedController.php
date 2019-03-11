da34d66adcf8cd9d22784bbb14a6cabc6ffbc12b
Matthew Timoshenko <matvejs.timosenko@accenture.com>
2019-03-11 09:49:47 +0200

    Adding feed parsing via js

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FeedController extends AbstractController
@@ -12,8 +13,12 @@ class FeedController extends AbstractController
     public function index()
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
-        $feed = 'https://www.theregister.co.uk/software/headlines.atom';
-        $arrayFeed = (array) simplexml_load_file($feed);
+        try {
+            $feedUrl = 'https://www.theregister.co.uk/software/headlines.atom';
+            $arrayFeed = (array) simplexml_load_file($feedUrl);
+        } catch (\Exception $e) {
+            return new Response($e->getMessage());
+        }
 
         return $this->render('feed/index.html.twig', [
             'feed' => $arrayFeed['entry'],
